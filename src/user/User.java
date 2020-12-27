bdcea9baf20359fcbc733e11796db42565dbe0ff
pjy <450857746@qq.com>
2020-12-27 17:56:12 +0800

    测试
    Signed-off-by: pjy <450857746@qq.com>

---
+++ src/user/User.java
@@ -0,0 +1,15 @@
+package user;
+
+public class User {
+    public void setUsername(String value) {
+    }
+
+    public void setPassword(String value) {
+    }
+
+    public void setAddress(String value) {
+    }
+
+    public void setPhone(String value) {
+    }
+}
