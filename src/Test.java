4eb2bf2d74ae7233a0661f71a0db4efabc95c374
pjy <450857746@qq.com>
2020-12-27 22:35:33 +0800

    读取excel
    Signed-off-by: pjy <450857746@qq.com>

--- src/Test.java
+++ src/Test.java
@@ -1,15 +1,36 @@
+import user.User;
+
+import java.io.File;
 import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args){
-        System.out.println("请输入用户名:");
+    public static void main(String[] args) {
+        /*
+        开始读文件
+         */
+        
+        System.out.println("请输入用户名：");
 
-        Scanner sc=new Scanner(System.in);
-        String username=sc.next();
-        System.out.println("请输入你的用户名:"+username);
+        Scanner sc = new Scanner(System.in);
+        String username = sc.next();//阻塞方法
+        System.out.println("你输入的用户名：" + username);
 
-        System.out.println("请输入密码:");
-        String  password=sc.next();
-        System.out.println("你输入的密码是:" + password);
+        System.out.println("请输入密码：");
+        String password = sc.next();
+
+        System.out.println("你输入的密码是：" + password);
+
+        File file = new File("C:\\Users\\庞\\IdeaProjects\\CmdShop\\src\\users.xlsx");
+        ReadExcel readExcel = new ReadExcel();//创建对象
+        User users[] = readExcel.readExcel(file);
+
+        for (int i = 0; i < users.length; i++) {
+            if (username.equals(users[i].getUsername()) && password.equals(users[i].getPassword())) {
+                System.out.println("登陆成功");
+                break;
+            } else {
+                System.out.println("登陆失败");
+            }
+        }
     }
 }
--- src/user/User.java
+++ src/user/User.java
@@ -1,15 +1,40 @@
 package user;
 
 public class User {
-    public void setUsername(String value) {
+    private String username;
+    private String password;
+    private String address;
+    private String phone;
+
+    public String getUsername() {
+        return username;
     }
 
-    public void setPassword(String value) {
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public void setAddress(String value) {
+    public String getPassword() {
+        return password;
     }
 
-    public void setPhone(String value) {
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 }
